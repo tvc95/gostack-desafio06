c5d1c30ae543cab2f5ac01bd8b0a2f0e38e9983f
tvc95 <thiagovc1500@gmail.com>
2021-03-04 15:19:51 -0300

    Solução do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
@@ -10,8 +11,41 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(transactionsList: Transaction[]): Promise<Balance> {
+    const incomeReducer = (
+      acc: number,
+      currentValue: Transaction,
+      index: number,
+      array: Transaction[],
+    ) => {
+      if (array[index].type === 'income') {
+        return acc + Number(currentValue.value);
+      }
+      return acc;
+    };
+
+    const outcomeReducer = (
+      acc: number,
+      currentValue: Transaction,
+      index: number,
+      array: Transaction[],
+    ) => {
+      if (array[index].type === 'outcome') {
+        return acc + Number(currentValue.value);
+      }
+      return acc;
+    };
+
+    const totalIncome = transactionsList.reduce(incomeReducer, 0);
+    const totalOutcome = transactionsList.reduce(outcomeReducer, 0);
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const transactionsList = await transactionsRepository.find();
+    const { total } = await transactionsRepository.getBalance(transactionsList);
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError(
+        'Transação inválida: valor da transação maior que o saldo do usuário!',
+        400,
+      );
+    }
+
+    let foundCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!foundCategory) {
+      foundCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(foundCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: foundCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
